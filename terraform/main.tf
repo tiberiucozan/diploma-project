329cdbef2f5f7c6ec8fdcba52b7fc303cfa582b6
Tiberiu Cozan <tibi.cozan@outlook.com>
2023-06-22 17:38:45 +0300

    change size

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,7 @@ locals {
   network_name = "default"
 }
 
+# https://github.com/nlamirault/terraform-civo-kubernetes
 module "civo-argo-cluster" {
   source       = "github.com/nlamirault/terraform-civo-kubernetes?ref=v0.3.0"
   cluster_name = "ArgoCD-Cluster"
@@ -14,10 +15,11 @@ module "civo-argo-cluster" {
   tags = local.tags
 }
 
+# https://github.com/nlamirault/terraform-civo-kubernetes
 module "civo-flux-cluster" {
   source       = "github.com/nlamirault/terraform-civo-kubernetes?ref=v0.3.0"
   cluster_name = "FLux-Cluster"
-  node_count   = 3
+  node_count   = 2
   region       = local.region
   network_name = local.network_name
 
